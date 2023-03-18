551b9f4cf25a4038bc80cd36137bf9076e975bf3
lucia cappll <luciacappll@gmail.com>
2023-03-18 11:32:15 -0300

    cards servicios

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from '../components/Navbar.jsx';
 import Footer from '../components/Footer.jsx';
 import Home from '../views/Home.jsx';
@@ -19,7 +19,7 @@ const Router = ()=>{
                 <Route path='/portafolio' element={<Portafolio/>}/>
                 <Route path='/servicios' element={<Servicios/>}/>
             </Routes>
-            {/* <Footer/> */}
+            <Footer/>
         </BrowserRouter>
     )
 }
--- src/views/Servicios.jsx
+++ src/views/Servicios.jsx
@@ -3,7 +3,12 @@ import '../assets/styles/Servicios.css'
 import laptop from '../assets/gallery/servicios_laptop.png';
 import paint from '../assets/gallery/servicios_paint.png';
 import photos from '../assets/gallery/servicios_photos.png';
-
+import dashboard from '../assets/gallery/servicios_dashboard.png';
+import phoneLink from '../assets/gallery/servicios_phonelink.png';
+import colorlens from '../assets/gallery/servicios_colorlens.png';
+import apple from '../assets/gallery/servicios_apple.png';
+import camera from '../assets/gallery/servicios_camera.png';
+import instagram from '../assets/gallery/servicios_instagram.png';
 
 const Servicios = () => {
   return (
@@ -12,27 +17,57 @@ const Servicios = () => {
       <h1>Servicios</h1>
       <div class="card-deck">
         <div class="card">
-          <img class="card-img-top servicios_img" src={laptop} alt="laptop" style={{width:"221.78px", height:"97.56px"}}/>
+          <img class="card-img-top" src={laptop} alt="laptop" style={{width:"221.78px", height:"97.56px", position:"relative", top:"25px"}}/>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <h5 class="card-title">Web design</h5>
+            <p class="card-text">Diseño de aplicaciones y páginas web partiendo desde el diseño hasta el desarrollo de la plataforma</p>
+            <div className="card-features">
+              <div className="card-feature">
+                <img src={dashboard} alt="dashboard" className="card-feature-icon" style={{width:"16.23px", height:"16.23px"}}/>
+                <span className="card-feature-txt">UX/UI Design</span>
+              </div>
+              <div className="card-feature">
+                <img src={phoneLink} alt="phonelink" className="card-feature-icon" style={{width:"22.72px", height:"22.72px"}}/>
+                <span className="card-feature-txt">Responsive</span>
+              </div>
+            </div>
+            <button className='card-btn'>Ver Mas</button>
           </div>
-        </div>
+        </div>  
         <div class="card">
-          <img class="card-img-top servicios_img" src={paint} alt="paint" style={{width:"137.55px", height:"126.26px"}}/>
+          <img class="card-img-top servicios_img" src={paint} alt="laptop" style={{width:"137.55px", height:"126.26px"}}/>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <h5 class="card-title">Branding</h5>
+            <p class="card-text">Diseñamos la identidad de marca para tu negocio, desde el enfoque estético, comercial y marketing.</p>
+            <div className="card-features">
+              <div className="card-feature">
+                <img src={apple} alt="dashboard" className="card-feature-icon" style={{width:"25.25px", height:"25.25px"}}/>
+                <span className="card-feature-txt">Logotipos</span>
+              </div>
+              <div className="card-feature">
+                <img src={colorlens} alt="phonelink" className="card-feature-icon" style={{width:"27.86px", height:"27.86px"}}/>
+                <span className="card-feature-txt">Colorimetría</span>
+              </div>
+            </div>
+            <button className='card-btn'>Ver Mas</button>
           </div>
-        </div>
+        </div>  
         <div class="card">
-          <img class="card-img-top servicios_img" src={photos} alt="photos" style={{width:"137.34px", height:"126.07px"}}/>
+          <img class="card-img-top servicios_img" src={photos} alt="laptop" style={{width:"137.34px", height:"126.06px"}}/>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <h5 class="card-title">Redes Sociales</h5>
+            <p class="card-text">Manejo de redes sociales y creación de contenido para plataformas digitales y redes sociales.</p>
+            <div className="card-features">
+              <div className="card-feature">
+                <img src={instagram} alt="dashboard" className="card-feature-icon" style={{width:"16.23px", height:"16.23px"}}/>
+                <span className="card-feature-txt">Plataformas</span>
+              </div>
+              <div className="card-feature">
+                <img src={camera} alt="phonelink" className="card-feature-icon" style={{width:"22.72px", height:"22.72px"}}/>
+                <span className="card-feature-txt">Contenido</span>
+              </div>
+            </div>
+            <button className='card-btn'>Ver Mas</button>
           </div>
         </div>  
       </div>
